c02cc8d8f04171d475a9e70a3066c78e1112039a
Zachary TW Pinto <zachwalker.pinto@gmail.com>
2024-01-18 20:58:03 -0500

    manufacture_inventory_sales_ratio.csv

---
+++ src/data/extract/lagging_indicators/manufacture_inventory_sales_ratio.py
@@ -0,0 +1,28 @@
+from fredapi import Fred
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+
+# Load environment variables
+load_dotenv()
+
+# Get API key from environment variable
+api_key = os.getenv("FRED_API_KEY")
+
+# Initialize Fred with your API key
+fred = Fred(api_key=api_key)
+
+# Fetch data series
+data = fred.get_series("MNFCTRIRSA")
+
+# Convert to DataFrame
+df = pd.DataFrame(data, columns=['Value'])
+
+# Filter data to only include the last 10 years
+ten_years_ago = datetime.now() - pd.DateOffset(years=10)
+df = df[df.index >= ten_years_ago]
+
+# Save to CSV
+df.to_csv("/Users/pintoza/Desktop/dev/analytics-engineering/real-time-indicators/"
+          "data/raw/lagging_indicators/manufacture_inventory_sales_ratio.csv")
