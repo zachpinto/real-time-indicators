76a179ac0739f0acfc1fdb7deb21609d50385fe2
Zachary TW Pinto <zachwalker.pinto@gmail.com>
2024-01-19 19:30:45 -0500

    avg_weekly_initial_jobless_claims.csv

---
+++ src/data/preprocessing/leading_indicators/avg_weekly_initial_jobless_claims.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Path to the raw data file
+raw_data_file = '/Users/pintoza/Desktop/dev/analytics-engineering/real-time-indicators/data/raw/leading_indicators/avg_weekly_initial_jobless_claims.csv'
+
+# Load the file without headers
+df = pd.read_csv(raw_data_file)
+
+# Rename the columns
+df.columns = ['date', 'value']
+
+# Convert the date column to datetime
+df['date'] = pd.to_datetime(df['date'])
+
+# Save the file
+df.to_csv('/Users/pintoza/Desktop/dev/analytics-engineering/real-time-indicators/data/processed/leading_indicators/avg_weekly_initial_jobless_claims.csv', index=False)
