9f47ae99cbe8d68f442f5d8a341533bb9f79b2a6
Zachary TW Pinto <zachwalker.pinto@gmail.com>
2024-01-19 22:22:10 -0500

    loaded data to pg db

---
+++ src/data/load/leading_indicators/building_permits.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+db_name = os.getenv('DB_NAME')
+
+# Establish a connection to the database
+engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
+
+# Path to your CSV file
+csv_file_path = '/Users/pintoza/Desktop/dev/analytics-engineering/real-time-indicators/data/processed/leading_indicators/building_permits.csv'
+
+# Load data from CSV file into DataFrame
+data_df = pd.read_csv(csv_file_path)
+
+# Table name in the database
+table_name = 'building_permits'
+
+# Load data from DataFrame to PostgreSQL table
+data_df.to_sql(table_name, engine, if_exists='replace', index=False)
+
+print("Data loaded successfully")
